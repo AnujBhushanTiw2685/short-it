9871be8715203ba8a9df788a7ae1a0bb5b65ee11
Anuj Bhushan Tiwari <anujbhushantiwari2022@vitbhopal.ac.in>
2025-08-15 18:36:12 +0530

    Update axiosInstance.js

--- URL_SHORTNER-master/FRONTEND/src/utils/axiosInstance.js
+++ URL_SHORTNER-master/FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"https://short-it-5pt5.onrender.com",
+    baseURL:"https://short-it-m7p4.onrender.com",
     timeout:10000, //10s
     withCredentials:true
 })
